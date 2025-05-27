b2ecc6a1da72d9f2f739d024ab56b2556c05e78d
Bilal Mohamed <104165822+bilal-tunchie@users.noreply.github.com>
2025-05-27 23:06:35 +0300

    Update Transform_crm_cust_info_table.sql

--- scripts/silver/Transform_crm_cust_info_table.sql
+++ scripts/silver/Transform_crm_cust_info_table.sql
@@ -3,22 +3,22 @@
 ---- Check for Nulls or duplicates in primary key (cst_id)
 
 SELECT 
-cst_id, COUNT(*)
-FROM bronze.crm_cust_info
+	cst_id, COUNT(*)
+FROM Silver.crm_cust_info
 GROUP BY cst_id
-HAVING COUNT(*) > 1
+HAVING COUNT (*) > 1 OR cst_id IS NULL
 
 ---- Check for unwanted spaces (cst_firstName, cst_lastName)
 
 SELECT 
-cst_firstName
+	cst_firstName
 FROM bronze.crm_cust_info
 WHERE cst_firstName != trim(cst_firstName)
 
 ---- Data Standarization & Consistency (cst_martial_status, cst_gndr)
 
 SELECT DISTINCT
-cst_martial_status
+	cst_martial_status
 FROM bronze.crm_cust_info;
 
 
@@ -33,25 +33,26 @@ FROM bronze.crm_cust_info;
 ---- Expectation: No Result
 
 SELECT 
-cst_id,
-cst_key, 
-trim(cst_firstName) AS cst_firstName,
-trim(cst_lastName) AS cst_lastName,
-CASE 
-	WHEN UPPER(TRIM(cst_martial_status)) = 'M' THEN 'Married' 
-	WHEN UPPER(TRIM(cst_martial_status)) = 'S' THEN 'Single' 
-	ELSE 'n/a'
-END AS cst_martial_status,
-CASE 
-	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' 
-	WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female' 
-	ELSE 'n/a'
-END AS cst_gndr,
-cst_create_date
+	cst_id,
+	cst_key, 
+	trim(cst_firstName) AS cst_firstName,
+	trim(cst_lastName) AS cst_lastName,
+	CASE 
+		WHEN UPPER(TRIM(cst_martial_status)) = 'M' THEN 'Married' 
+		WHEN UPPER(TRIM(cst_martial_status)) = 'S' THEN 'Single' 
+		ELSE 'n/a'
+	END AS cst_martial_status,
+	CASE 
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male' 
+		WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female' 
+		ELSE 'n/a'
+	END AS cst_gndr,
+	cst_create_date
 FROM (
-SELECT 
-*,
-ROW_NUMBER() OVER(PARTITION BY cst_id order by cst_create_date DESC) as Flag_last
-FROM bronze.crm_cust_info
+	SELECT 
+		*,
+		ROW_NUMBER() OVER(PARTITION BY cst_id order by cst_create_date DESC) as Flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
 )t 
 WHERE Flag_last = 1
