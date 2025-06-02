dedeca5318518816200d381087711517c884349f
Bilal Mohamed <104165822+bilal-tunchie@users.noreply.github.com>
2025-06-02 22:30:05 +0300

    Update Transform_crm_prd_info_table.sql

--- scripts/silver/Transform_crm_prd_info_table.sql
+++ scripts/silver/Transform_crm_prd_info_table.sql
@@ -62,7 +62,7 @@ INSERT INTO Silver.crm_prd_info (
 SELECT 
 	prd_id,
 	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
-	REPLACE(SUBSTRING(prd_key, 7, LEN(prd_key)), '-', '_') AS prd_key,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
 	prd_nm,
 	ISNULL(prd_cost, 0) AS prd_cost,
 	CASE UPPER(TRIM(prd_line))
