2a2852a2a399860d84ecf7a73bbed9909f13af96
Bilal Mohamed <104165822+bilal-tunchie@users.noreply.github.com>
2025-07-02 23:11:50 +0300

    Update Data Exploration.sql

--- Data Analysis/Exploratory Data Analysis (EDA) /Data Exploration.sql
+++ Data Analysis/Exploratory Data Analysis (EDA) /Data Exploration.sql
@@ -11,7 +11,7 @@ WHERE TABLE_NAME = 'dim_customers'
 
 --************************************--
 
--- Dimension Exploration
+------ ****** Dimension Exploration ****** ------
 
 ---- Customers Dimensions
 SELECT * FROM gold.dim_customers
@@ -28,7 +28,8 @@ ORDER BY 1,2,3
 
 --************************************--
 
--- DATE Exploration 
+------ ****** DATE Exploration ****** ------
+	
 --- * Identify the earliest and latest dates (boundaries)
 --- * Understand the scope of data and timespan ( do we have 2 years or 10 years?)
 
@@ -44,7 +45,8 @@ FROM gold.fact_sales
 
 --************************************--
 
--- Measures Exploration
+------ ****** Measures Exploration ****** ------
+	
 --- Calculate the key metrics of the business (Calculate the highest and lowest level of aggregation)
 
 --- Total Sales
@@ -94,6 +96,8 @@ UNION ALL
 SELECT 'Total Products', FORMAT(COUNT(product_key), 'N0') FROM gold.dim_products
 UNION ALL
 SELECT 'Total Customers', FORMAT(COUNT(customer_key), 'N0') FROM gold.dim_customers
+
+--************************************--
 
 ------ ****** Magnitude ****** ------
 
@@ -159,6 +163,8 @@ ON c.customer_key = s.customer_key
 GROUP BY c.country
 ORDER BY total_customer_rev_by_country DESC
 
+--************************************--
+
 ------ ****** Ranking ****** ------
 
 ---- Which 5 products generate the highest revenue?
@@ -183,7 +189,10 @@ LEFT JOIN  gold.dim_products p
 ON p.product_key = s.product_key
 GROUP BY p.product_name
 
+--************************************--
+
 ------ ****** Change over time ****** ------
+	
 ---- Analyze sales performance over time
 
 SELECT 
@@ -197,7 +206,10 @@ GROUP BY YEAR(order_date), MONTH(order_date)
 HAVING MONTH(order_date) IS NOT NULL
 ORDER BY years, months
 
+--************************************--
+
 ------ ****** Cumulative Analysis ****** ------
+	
 ---- Aggregate the data progressively over time
 
 SELECT *,SUM(rev_by_years) OVER(PARTITION BY year(month_)  ORDER BY month_)
@@ -211,7 +223,10 @@ FROM (
 	--ORDER BY month_
 )t
 
+--************************************--
+
 ------ ****** Performance Analysis ****** ------
+	
 /*---- Analyze the yearly performance of products by comparing their sales
  to both the average sales performance of the product and the previous year's sales */
 
@@ -246,3 +261,93 @@ SELECT
 	END AS avg_status
 FROM yearly_products_sales 
 ORDER BY product_name, order_year
+
+--************************************--
+
+------ ****** Part-to-whole ****** ------
+	
+/*---- Analyze how an individual part is performing compared to the overall,
+allowing us to understand which category has the greatest impact on the business. */
+
+---- Which categories contribute the most to overall sales?
+SELECT 
+	category,
+	sales_by_category,
+	sum(sales_by_category) OVER() Total_sales,
+	CONCAT(ROUND(CAST(sales_by_category AS FLOAT) / sum(sales_by_category) OVER() * 100, 2), '%')  AS percentage_
+FROM(
+	SELECT 
+		p.category,
+		sum(f.sales_amount) sales_by_category
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON f.product_key = p.product_key
+	GROUP BY p.category
+)t
+ORDER BY sales_by_category DESC
+
+--************************************--
+
+------ ****** Data Segmentation ****** ------
+	
+/*---- Group the data based on a specific range.
+Helps understand the correlation between two measures. ----*/
+
+/*---- Segment products into cost ranges and
+count how many products fall into each segment. ----*/
+
+SELECT *, COUNT(product_name) OVER(PARTITION BY SEGMENT) AS number_of_products_by_segment
+FROM(
+	SELECT 
+		product_name,
+		cost,
+		CASE
+			WHEN cost BETWEEN 0 AND 750 THEN 'LOW'
+			WHEN cost BETWEEN 750 AND 1500 THEN 'MEDIUM'
+			WHEN cost > 1500  THEN 'HIGH'
+		END AS segment
+	
+	FROM gold.dim_products
+)t
+ORDER BY cost
+
+/*---- 
+Group customers into three segments based on their spending behavior:
+
+- VIP: at least 12 months of history and spending more than €5,000.
+- Regular: at least 12 months of history but spending €5,000 or less.
+- New: lifespan less than 12 months.
+
+And find the total number of customers by each group.
+----*/
+
+WITH customer_spending AS (
+	SELECT
+		c.customer_key,
+		SUM(f.sales_amount) AS total_spending,
+		MIN(f.order_date) AS first_order,
+		MAX(f.order_date) AS last_order,
+		DATEDIFF( month, MIN(f.order_date), MAX(f.order_date) ) AS lifespan
+	FROM gold.fact_sales f
+	LEFT JOIN gold.dim_customers c
+	ON f.customer_key = c.customer_key
+	GROUP BY c.customer_key
+)
+
+SELECT
+	customer_segment,
+	COUNT(customer_key) AS total_customers
+FROM(
+	SELECT
+		customer_key,
+		total_spending,
+		lifespan,
+		CASE
+			WHEN total_spending > 5000 AND lifespan >= 12 THEN 'VIP'
+			WHEN total_spending <= 5000 AND lifespan >= 12 THEN 'Regular'
+			ELSE 'NEW'
+		END AS customer_segment
+	FROM customer_spending
+)t3
+GROUP BY customer_segment
+ORDER BY total_customers DESC
